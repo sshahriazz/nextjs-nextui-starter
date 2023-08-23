f5a2ee3885796e0b50e33b68c14194d4561b0d97
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2023-08-23 15:33:18 +0600

    next-auth added

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,19 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  // Secret for Next-auth, without this JWT encryption/decryption won't work
+  secret: process.env.NEXTAUTH_SECRET,
+
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_APP_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
